5d293d0dd2df6d0a1c11a83d93b41f3c92ce72f0
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-24 18:23:41 +0000

    add BIND_ADDRESS in addition to HOSTNAME

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ console.log(config({ export: true}));
 
 const DENO_PORT = parseInt(Deno.env.get('DENO_PORT'));
 const HOSTNAME = Deno.env.get('HOSTNAME');
+const BIND_ADDRESS = Deno.env.get('BIND_ADDRESS');
 const REDIS_HOSTNAME = Deno.env.get('REDIS_HOSTNAME');
 const REDIS_PORT = parseInt(Deno.env.get('REDIS_PORT'));
 const REDIS_PASSWORD = Deno.env.get('REDIS_PASSWORD');
 
-const s = serve({ hostname: HOSTNAME, port: DENO_PORT});
+const s = serve({ hostname: BIND_ADDRESS, port: DENO_PORT});
+console.log("BIND_ADDRESS:" + BIND_ADDRESS);
 console.log("http://:" + HOSTNAME + ":" + DENO_PORT);
 
 const encoder = new TextEncoder();
